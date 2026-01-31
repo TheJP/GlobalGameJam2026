a52e3fb1c7bc36a038fd8427b7a3bbb4f7fba3d2
TheJP <jp_55555@hotmail.com>
2026-01-31 18:00:22 +0100

    Implemented simple dialog system

---
+++ GlobalGameJam2026/Assets/Scripts/DialogSystem.cs
@@ -0,0 +1,275 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public enum Who
+{
+    Player, Npc,
+}
+
+public enum Dialog
+{
+    IntroOldRoommate,
+    CamVampire,
+    CamKawai,
+    CamGamer,
+    CamCrypto,
+    CamNeighbour,
+    CamSock,
+}
+
+public enum NodeIndex
+{
+    Greeting,
+
+    PlayerQuestion,
+    NpcAnyQuestion,
+
+    NpcAnswer1_1,
+    NpcAnswer1_2,
+    NpcAnswer2_1,
+    NpcAnswer3_1,
+
+    NpcQuestion1,
+    NpcQuestion1_Reaction1,
+    NpcQuestion1_Reaction2,
+    NpcQuestion2,
+    NpcQuestion2_Reaction1,
+    NpcQuestion2_Reaction2,
+    NpcQuestion3,
+    NpcQuestion3_Reaction1,
+    NpcQuestion3_Reaction2,
+
+    End,
+}
+
+public static class DialogSystem
+{
+    private static PlayerQuestionNode VampireQuestions = new(
+        new PlayerQuestion("Ask about his job", "So, you work at a gas station, huh? What’s that like?", NodeIndex.NpcAnswer1_1),
+        new PlayerQuestion("Ask about his intentions", "Why do you want to live in a shared flat with me?", NodeIndex.NpcAnswer2_1)
+    );
+
+    public static Dictionary<Dialog, DialogTree> Dialogs { get; } = new()
+    {
+        [Dialog.CamVampire] = new(new()
+        {
+            [NodeIndex.Greeting] = new SimpleNode(NodeIndex.PlayerQuestion, Who.Npc, "Good day, my dear one. I’d like to introduce myself: My name is Wilfred Novak, but you can call me Willy. I’m excited to make your acquaintance."),
+
+            [NodeIndex.PlayerQuestion] = VampireQuestions,
+            [NodeIndex.NpcAnyQuestion] = new NpcAnyQustionNode(NodeIndex.NpcQuestion1, NodeIndex.NpcQuestion2),
+
+            [NodeIndex.NpcAnswer1_1] = new OptionsNode("It’s quiet. I work at night, so I won’t disturb your slumber. But I’d love to be a детский комбайн. Ah, I mean a kindergardener.", new SimpleOption("Do you mean a kindergarten teacher?", NodeIndex.NpcAnswer1_2)),
+            [NodeIndex.NpcAnswer1_2] = new SimpleNode(NodeIndex.NpcAnyQuestion, Who.Npc, "Da, of course. Children are delicious , um, delightful."),
+
+            [NodeIndex.NpcAnswer2_1] = new SimpleNode(NodeIndex.NpcAnyQuestion, Who.Npc, "I'm looking for new comrades. Life in my residence has been rather dull. I seek for new connections to share refreshments with. Or whatever it is people do these days."),
+
+            [NodeIndex.NpcQuestion1] = new OptionsNode("I appreciate the company of fellow humans. Do mortals frequently visit this habitat?",
+                new SimpleOption("Yes, my friends and I love to hang out in my living room.", NodeIndex.NpcQuestion1_Reaction1),
+                new SimpleOption("No, I’m a lone wolf.", NodeIndex.NpcQuestion1_Reaction2)
+            ),
+            [NodeIndex.NpcQuestion1_Reaction1] = new SimpleNode(NodeIndex.PlayerQuestion, Who.Npc, "Wonderful..."),
+            [NodeIndex.NpcQuestion1_Reaction2] = new SimpleNode(NodeIndex.PlayerQuestion, Who.Npc, " Oh… That’s too bad."),
+
+            [NodeIndex.NpcQuestion2] = new OptionsNode("How is the parking situation? I wish to bring my automobiles with me.",
+                new SimpleOption("There are a lot of parking spaces. I’ll ring the landlord to set you up with some.", NodeIndex.NpcQuestion2_Reaction1),
+                new SimpleOption("Cars? Plural? Good luck with that, I can’t even get a space for my own ride.", NodeIndex.NpcQuestion2_Reaction2)
+            ),
+            [NodeIndex.NpcQuestion2_Reaction1] = new SimpleNode(NodeIndex.PlayerQuestion, Who.Npc, "You’re too kind my cherished friend."),
+            [NodeIndex.NpcQuestion2_Reaction2] = new SimpleNode(NodeIndex.PlayerQuestion, Who.Npc, "That’s unfortunate."),
+        }),
+    };
+}
+
+public class DialogTree
+{
+    public Dictionary<NodeIndex, IDialogNode> Nodes { get; private set; }
+    public string InterruptMessage { get; set; }
+
+    public DialogTree(Dictionary<NodeIndex, IDialogNode> nodes, string interruptMessage = null)
+    {
+        Nodes = nodes;
+        InterruptMessage = interruptMessage;
+    }
+}
+
+public interface IDialogNode
+{
+    void Enter(DialogStateMachine stateMachine);
+    void OptionSelected(DialogStateMachine stateMachine, int selectedOption) { }
+}
+
+public class SimpleNode : IDialogNode
+{
+    public Who Who { get; }
+    public string Prompt { get; }
+    public NodeIndex NextNode { get; }
+
+    public SimpleNode(NodeIndex nextNode, Who who, string prompt)
+    {
+        Who = who;
+        Prompt = prompt;
+        NextNode = nextNode;
+    }
+
+    public void Enter(DialogStateMachine stateMachine)
+    {
+        stateMachine.ShowDialog(Who, Prompt);
+        stateMachine.Transition(NextNode);
+    }
+}
+
+public class OptionsNode : IDialogNode
+{
+    public string Prompt { get; private set; }
+    public SimpleOption[] Options { get; private set; }
+    public IList<string> Answers => Options.Select(o => o.Text).ToList();
+
+    public void Enter(DialogStateMachine stateMachine)
+    {
+        stateMachine.ShowDialog(Who.Npc, Prompt);
+        stateMachine.ShowOptions(Answers);
+    }
+
+    public void OptionSelected(DialogStateMachine stateMachine, int selectedOption) => stateMachine.Transition(Options[selectedOption].NextNode);
+
+    public OptionsNode(string prompt, params SimpleOption[] options)
+    {
+        Prompt = prompt;
+        Options = options;
+    }
+}
+
+public record SimpleOption
+{
+    public string Text { get; }
+    public NodeIndex NextNode { get; }
+
+    public SimpleOption(string answer, NodeIndex nextNode)
+    {
+        Text = answer;
+        NextNode = nextNode;
+    }
+}
+
+public class NpcAnyQustionNode : IDialogNode
+{
+    private IList<NodeIndex> PossibleQuestions { get; }
+
+    public NpcAnyQustionNode(params NodeIndex[] possibleQuestions)
+    {
+        PossibleQuestions = possibleQuestions;
+    }
+
+    public void Enter(DialogStateMachine stateMachine)
+    {
+        if (!stateMachine.Data.ContainsKey(DataIndex.NpcAskedQuestions))
+        {
+            stateMachine.Data.Add(DataIndex.NpcAskedQuestions, new HashSet<NodeIndex>());
+
+        }
+        var askedQuestions = stateMachine.Data[DataIndex.NpcAskedQuestions] as ISet<NodeIndex>;
+
+        var openQuestions = PossibleQuestions.Where(q => !askedQuestions.Contains(q)).ToList();
+        if (openQuestions.Count == 0)
+        {
+            stateMachine.Transition(NodeIndex.End);
+        }
+        else
+        {
+            var node = openQuestions[Random.Range(0, openQuestions.Count)];
+            askedQuestions.Add(node);
+            stateMachine.Transition(node);
+        }
+    }
+}
+
+public class PlayerQuestionNode : IDialogNode
+{
+    public PlayerQuestion[] PossibleOptions { get; }
+    private IList<PlayerQuestion> LastShownOptions { get; set; }
+
+    public PlayerQuestionNode(params PlayerQuestion[] possibleOptions)
+    {
+        PossibleOptions = possibleOptions;
+    }
+
+    public void Enter(DialogStateMachine stateMachine)
+    {
+        if (!stateMachine.Data.ContainsKey(DataIndex.PlayerAskedQuestions))
+        {
+            stateMachine.Data.Add(DataIndex.PlayerAskedQuestions, new HashSet<PlayerQuestion>());
+        }
+        var askedQuestions = stateMachine.Data[DataIndex.PlayerAskedQuestions] as ISet<PlayerQuestion>;
+
+        var openQuestions = PossibleOptions.Where(q => !askedQuestions.Contains(q)).ToList();
+        if (openQuestions.Count == 0)
+        {
+            stateMachine.Transition(NodeIndex.End);
+        }
+        else
+        {
+            LastShownOptions = openQuestions;
+            stateMachine.ShowOptions(openQuestions.Select(o => o.Option).ToList());
+        }
+    }
+
+    public void OptionSelected(DialogStateMachine stateMachine, int selectedOption)
+    {
+        var option = LastShownOptions[selectedOption];
+        stateMachine.ShowDialog(Who.Player, option.Text);
+        stateMachine.Transition(option.NextNode);
+    }
+}
+
+public class PlayerQuestion
+{
+    public string Option { get; }
+    public string Text { get; }
+    public NodeIndex NextNode { get; }
+
+    public PlayerQuestion(string option, string text, NodeIndex nextNode)
+    {
+        Option = option;
+        Text = text;
+        NextNode = nextNode;
+    }
+}
+
+public enum DataIndex
+{
+    NpcAskedQuestions,
+    PlayerAskedQuestions,
+}
+
+public class DialogStateMachine
+{
+    public event System.Action<Who, string> OnShowDialog;
+    public event System.Action<IList<string>> OnShowOptions;
+
+    public DialogTree DialogTree { get; private set; }
+    public NodeIndex Node { get; set; }
+
+    /// <summary>Data that lives for the duration of the dialog.</summary>
+    public Dictionary<DataIndex, object> Data { get; } = new();
+
+    public IDialogNode DialogNode => DialogTree.Nodes[Node];
+
+    public void StartDialog(Dialog dialog, NodeIndex startNode = 0) => StartDialog(DialogSystem.Dialogs[dialog], startNode);
+
+    public void StartDialog(DialogTree dialogTree, NodeIndex startNode = 0)
+    {
+        DialogTree = dialogTree;
+        Node = startNode;
+        Data.Clear();
+        Transition(startNode);
+    }
+
+    public void ShowDialog(Who who, string message) => OnShowDialog?.Invoke(who, message);
+    public void ShowOptions(IList<string> options) => OnShowOptions?.Invoke(options);
+    public void Transition(NodeIndex nextNode)
+    {
+        Node = nextNode;
+        DialogNode.Enter(this);
+    }
+}
--- GlobalGameJam2026/Assets/Scripts/SubtitleConversation.cs
+++ GlobalGameJam2026/Assets/Scripts/SubtitleConversation.cs
@@ -1,13 +1,31 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 [RequireComponent(typeof(UIDocument))]
 public class SubtitleConversation : MonoBehaviour
 {
-    UIDocument document;
-    VisualElement root;
-    VisualElement subtitlesParent;
+    [field: SerializeField]
+    public Dialog Dialog { get; set; }
+
+    [field: SerializeField]
+    public Font PlayerFont { get; set; }
+
+    [field: SerializeField]
+    public Font NpcFont { get; set; }
+
+    [field: SerializeField]
+    public Color PlayerColor { get; set; }
+
+    [field: SerializeField]
+    public Color NpcColor { get; set; }
+
+    private UIDocument document;
+    private VisualElement root;
+    private VisualElement subtitlesParent;
+
+    private readonly Queue<ICommand> commands = new();
 
     public IEnumerator Start()
     {
@@ -16,24 +34,105 @@ public class SubtitleConversation : MonoBehaviour
         subtitlesParent = root.Q("Subtitles");
         subtitlesParent.Clear();
 
+        var vampireStateMachine = new DialogStateMachine();
+        vampireStateMachine.OnShowDialog += (who, text) => commands.Enqueue(new TextCommand(who, text));
+        vampireStateMachine.OnShowOptions += (options) => commands.Enqueue(new OptionsCommand(options));
+        vampireStateMachine.StartDialog(Dialog.CamVampire);
+
+
+        int? selectedOption = null;
+        ICommand currentCommand = null;
+
         while (true)
         {
-            yield return new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(0.15f);
 
-            string word = "";
-            for (int i = Random.Range(3, 10); i >= 0; --i)
+            if (currentCommand == null)
             {
-                word += (char)Random.Range((int)'a', (int)'z' + 1);
+                if (commands.Count == 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    currentCommand = commands.Dequeue();
+                    if (currentCommand is TextCommand)
+                    {
+                        subtitlesParent.Clear();
+                    }
+                }
             }
 
-            var label = new Label(word);
-            label.style.marginLeft = new(20f);
-            label.style.opacity = new(0f);
-            subtitlesParent.Add(label);
+            if (currentCommand is TextCommand textCommand)
+            {
+                if (textCommand.Text.Count == 0)
+                {
+                    currentCommand = null;
+                    //var lineBreak = new VisualElement();
+                    //lineBreak.style.width = new(new Length(100f, LengthUnit.Percent));
+                    //subtitlesParent.Add(lineBreak);
+                    continue;
+                }
 
-            yield return null;
-            label.style.marginLeft = new(0f);
-            label.style.opacity = new(1f);
+                var word = textCommand.Text.Dequeue();
+                var label = new Label(word);
+                label.style.marginLeft = new(20f);
+                label.style.opacity = new(0f);
+                if (textCommand.Who == Who.Npc)
+                {
+                    label.style.color = new(NpcColor);
+                    label.style.unityFont = NpcFont;
+                }
+                else if (textCommand.Who == Who.Player)
+                {
+                    label.style.color = new(PlayerColor);
+                    label.style.unityFont = PlayerFont;
+                }
+                subtitlesParent.Add(label);
+
+                yield return null;
+                label.style.marginLeft = new(0f);
+                label.style.opacity = new(1f);
+
+                var trimmedWord = word.Trim();
+                if (trimmedWord.EndsWith('.') || trimmedWord.EndsWith('!') || trimmedWord.EndsWith('?') || trimmedWord.EndsWith(':') || trimmedWord.EndsWith(';'))
+                {
+                    yield return new WaitForSeconds(0.5f);
+                }
+
+            }
+            else if (currentCommand is OptionsCommand optionsCommand)
+            {
+                // TODO
+                Debug.Log(optionsCommand.Options);
+            }
+
+            //string word = "";
+            //for (int i = Random.Range(3, 10); i >= 0; --i)
+            //{
+            //    word += (char)Random.Range((int)'a', (int)'z' + 1);
+            //}
         }
     }
+}
+
+interface ICommand { }
+
+class TextCommand : ICommand
+{
+    public Who Who { get; set; }
+    public Queue<string> Text { get; set; }
+
+    public TextCommand(Who who, string text)
+    {
+        Who = who;
+        Text = new(text.Split());
+    }
+}
+
+class OptionsCommand : ICommand
+{
+    public IList<string> Options { get; set; }
+
+    public OptionsCommand(IList<string> options) => Options = options;
 }
